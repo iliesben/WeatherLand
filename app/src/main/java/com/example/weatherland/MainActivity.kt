e7653ef598f8405c063bf534c8863f08ba29ebf1
iliesben <ilies.benosman@hetic.net>
2020-06-05 04:31:01 +0200

    interface setup

--- app/src/main/java/com/example/weatherland/MainActivity.kt
+++ app/src/main/java/com/example/weatherland/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    define('OPEN_WEATHER_MAP_API_KEY', '18e51876c8d93ab4a0bf1a25bcbb8fcd');
+    //define('OPEN_WEATHER_MAP_API_KEY', '18e51876c8d93ab4a0bf1a25bcbb8fcd');
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
